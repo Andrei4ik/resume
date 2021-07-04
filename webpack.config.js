a4795c2e0e13b007aa296f5f9de879a3ac84fa24
Andrei4ik <56185676+Andrei4ik@users.noreply.github.com>
2021-07-04 12:32:23 +0700

    подключение веб пака

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = request('path')
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[hash].js"
+    },
+    
+}
